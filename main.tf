a4c1c99359656bb7376d0fc8d34d10b7e9ebda80
Shota Yamamota <1230124fw@gmail.com>
2017-10-13 18:51:38 +0900

    Initial commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-d424e7b2"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "terraform-example"
+	}
+
+}
